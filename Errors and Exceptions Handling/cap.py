5b9943822b03905da7313ea9a476181515624d92
Tyler Coen <43471099+tylercoen@users.noreply.github.com>
2023-09-12 16:55:42 -0400

    Add files via upload

---
+++ Errors and Exceptions Handling/cap.py
@@ -0,0 +1,4 @@
+def cap_text(text):
+    # input a string
+    # output a string with the first letter capitalized
+    return text.title()
---
+++ Errors and Exceptions Handling/simple1.py
@@ -0,0 +1,12 @@
+# A VERY SIMPLE SCRIPT
+# to get the report, run this in the terminal: pylint simple1.py -ry
+def myFunc():
+    # A simple function
+
+    first = 1
+    second = 2
+    print(first)
+    print(second)
+
+
+myFunc()
